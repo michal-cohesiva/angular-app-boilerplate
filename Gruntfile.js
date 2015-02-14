d43257d3317071d6a5b61fa36c41df34ddb32afb
Michał Kłoczko <michal.kloczko@gmail.com>
2015-02-14 11:51:44 +0100

    fix typo in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -125,7 +125,7 @@ module.exports = function(grunt) {
                     base: [
                         '<%= paths.dev %>'
                     ],
-                    middlewares: middlewareConfig
+                    middleware: middlewareConfig
                 },
             }
 
